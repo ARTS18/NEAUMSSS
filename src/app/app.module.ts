c32263e9cae6e1f0341c742d905578a4fec4625c
Amber <amber.rutherford@voxlore.com>
2018-08-26 11:56:09 -0700

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import {
   MatInputModule,
@@ -9,18 +10,27 @@ import {
   MatToolbarModule,
   MatExpansionModule
 } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MatJumbotronModule } from '@angular-material-extensions/jumbotron';
+import {MatMenuModule} from '@angular/material/menu';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { NavComponent } from './header/nav/nav.component';
+import { SplashComponent } from './header/splash/splash.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    NavComponent,
+    SplashComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +40,10 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatMenuModule,
+    MatJumbotronModule.forRoot(),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post.model.ts
+++ src/app/posts/post.model.ts
@@ -1,4 +1,3 @@
 export interface Post {
   title: string;
-  content: string;
 }
